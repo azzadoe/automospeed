3dd49aa50f741f550b7876c6d346401765d3bb5b
Pediaku ID <azzaputra005@gmail.com>
2024-11-07 16:34:26 +0700

    fix generate press

--- wp-content/plugins/hester-core/themes/hester/customizer/settings/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once HESTER_CORE_PLUGIN_DIR . 'themes/hester/customizer/settings/settings-slider.php';
-require_once HESTER_CORE_PLUGIN_DIR . 'themes/hester/customizer/settings/settings-info.php';
-require_once HESTER_CORE_PLUGIN_DIR . 'themes/hester/customizer/settings/settings-services.php';
-require_once HESTER_CORE_PLUGIN_DIR . 'themes/hester/customizer/settings/settings-extra.php';
-require_once HESTER_CORE_PLUGIN_DIR . 'themes/hester/customizer/settings/settings-features.php';
-require_once HESTER_CORE_PLUGIN_DIR . 'themes/hester/customizer/settings/settings-blog.php';
-require_once HESTER_CORE_PLUGIN_DIR . 'themes/hester/customizer/settings/settings-products.php';
--- wp-content/themes/automobile-car-services/template-parts/pagination.php
+++
@@ -1,5 +0,0 @@
-<?php
-	the_posts_pagination( array(
-		'prev_text' => esc_html__( 'Previous page', 'automobile-car-services' ),
-		'next_text' => esc_html__( 'Next page', 'automobile-car-services' ),
-	) );
